3ebf18d58eca83a94d37d12f2fe8e14895b5e55c
CHITHRADEVI-98 <50815005+CHITHRADEVI-98@users.noreply.github.com>
2019-05-21 06:23:51 -0400

    Create med.c

---
+++ med.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+    int median,a[n],temp;
+    scanf("%d",&n);
+for(i=0;i<n;++i)
+scanf("%d",&a[i]);
+temp=0;
+for(i=0;i<n-1;i++)
+{
+    for(j=0;j<n-i-1;j++)
+    {
+        if(a[j]>a[j+1])
+        {
+            temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+            
+        }
+    }
+}
+if(n%2==0)
+median=(a[(n/2)-1]+a[(n/2)])/2;
+else
+median=a[(n/2)];
+printf("%d",median);
+}
