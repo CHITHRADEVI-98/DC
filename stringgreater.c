ae811db7ec070810fa859043f4a3c3ecb06f6d4b
CHITHRADEVI-98 <50815005+CHITHRADEVI-98@users.noreply.github.com>
2019-05-23 23:41:08 -0400

    Create stringgreater.c

---
+++ stringgreater.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  char str1[50],str2[50];
+  int n1,n2;
+  scanf("%s",&str1);
+  scanf("%s",&str2);
+  n1=strlen(str1);
+  n2=strlen(str2);
+  if(n1>n2)
+  {
+      printf("%s",str1);
+  }
+  else
+  
+  printf("%s",str2);
+}
